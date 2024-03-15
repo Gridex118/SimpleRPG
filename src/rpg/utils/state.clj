f7e72f99a162080fd3a4191e5635e0b5c0182dee
Alex Rosegrid <rosegrid58@gmail.com>
2024-03-15 20:00:03 +0530

    Change styling

--- src/rpg/utils/state.clj
+++ src/rpg/utils/state.clj
@@ -1,11 +1,11 @@
 (ns rpg.utils.state
   (:require clojure.java.io))
 
-(defn loadPlayer []
+(defn load-player []
   (let [sink "dump/player.edn"]
     (first (read-string (slurp sink)))))
 
-(defn savePlayer [playerMap]
+(defn save-player [playerMap]
   (let [sink "dump/player.edn"]
     (clojure.java.io/make-parents sink)
     (spit sink (with-out-str (pr [playerMap])))))
